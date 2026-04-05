0f00fd4d980f8cc0d5f3b3ae7e0e7eff604c8469
Ramavath Manjula <manjuramavath12@gmail.com>
2026-04-05 17:12:58 +0530

    arrays - assignment

---
+++ 01.Arrays/assignment.java
@@ -0,0 +1,87 @@
+import java.util.*;;
+
+public class assignment {
+    public int[] buildArray(int[] nums) {
+        int[] res = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = nums[nums[i]];
+        }
+        return res;
+    }
+
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = ans[i + n] = nums[i];
+        }
+        return ans;
+    }
+
+    public int[] runningSum(int[] nums) {
+        int[] runningSum = new int[nums.length];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            runningSum[i] = sum;
+        }
+        return runningSum;
+    }
+
+    public int maximumWealth(int[][] accounts) {
+        int max = Integer.MIN_VALUE;
+        for (int[] row : accounts) {
+            int curr = 0;
+            for (int i : row)
+                curr += i;
+            if (curr > max)
+                max = curr;
+        }
+        return max;
+    }
+
+    public int[] shuffle(int[] nums, int n) {
+        int[] ans = new int[2 * n];
+        for (int i = 0; i < n; i++) {
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[n + i];
+        }
+        return ans;
+    }
+
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> list = new ArrayList<>();
+        int max = Integer.MIN_VALUE;
+        for (int i : candies) {
+            max = Math.max(max, i);
+        }
+        for (int i = 0; i < candies.length; i++) {
+            list.add(candies[i] + extraCandies >= max);
+        }
+        return list;
+    }
+
+    public int numIdenticalPairs(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] == nums[j])
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int[] ans = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[j] < nums[i]) {
+                    ans[i]++;
+                }
+            }
+        }
+        return ans;
+    }
+}
