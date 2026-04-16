519b02bc2ac3de32befa0adc527e0eee98ebe6db
Ramavath Manjula <manjuramavath12@gmail.com>
2026-04-16 23:51:43 +0530

    searching assignment-medium

--- 02.Searching/SearchingAssignment.java
+++ 02.Searching/SearchingAssignment.java
@@ -337,4 +337,44 @@ public class SearchingAssignment {
         }
         return -1;
     }
+
+    public int search2(int[] nums, int target) {
+        int pivot = findPivot(nums);
+        int ans = search(nums, 0, pivot, target);
+        if (ans == -1) {
+            ans = search(nums, pivot + 1, nums.length - 1, target);
+        }
+        return ans;
+    }
+
+    public int findPivot(int[] nums) {
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < nums.length - 1 && nums[mid] > nums[mid + 1])
+                return mid;
+            else if (mid > 0 && nums[mid - 1] > nums[mid])
+                return mid - 1;
+            else {
+                if (nums[start] > nums[mid])
+                    end = mid - 1;
+                else
+                    start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int search(int[] nums, int start, int end, int target) {
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+        }
+        return -1;
+    }
 }
